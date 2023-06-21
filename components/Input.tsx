55a4e8da01ec1243c6b9d399de808266eca901be
Keyukemi <keyukemi.ku@gmail.com>
2023-06-21 18:59:20 +0100

    added schema

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,26 +1,48 @@
-import { InputHTMLAttributes } from "react";
+import React,{ InputHTMLAttributes } from "react";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps{
-
+    label: string;
+    id: string;
+    type?: string;
+    required?: boolean;
+    value: string;
+    onChange: any;
+    prefix?: () => React.ReactNode;
+    //register: UseFormRegister<FieldValues>,
+    errors?: FieldErrors
+   //disabled?: boolean;
+   
 }
 
 const Input = (props: InputHTMLAttributes<HTMLInputElement> & InputProps) => {
     return ( 
         <div className="relative">
-            <input
-                    className="
+            {
+                props.prefix && (
+                    props.prefix()
+                )
+            }
+            <input 
+                    {...props}
+                    className={`
                     block rounded-md w-full px-6 pt-6 pb-1 text-md text-paragraph peer
                     appearance-none focus:outline-none focus:ring-0 bg-primary
-                    "
+                    ${props.prefix? 'pl-9' : 'pl-4'} 
+                    ${props.errors? 'border-rose-500' : 'border-neutral-300'}
+                    ${props.errors? 'focus:border-rose-500' : 'focus:border-black'}
+                    `}
                     placeholder=" "
-                    id="email"
             />
             <label 
-                className="absolute text-md text-paragraph duration-150 transform scale-75
+                className={`absolute text-md text-paragraph duration-150 transform scale-75
                 -translate-y-3 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 
-                peer-placeholder-shown:-translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3" 
-                htmlFor="email" id="email">
-                    Email
+                peer-placeholder-shown:-translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3
+                ${props.errors? 'text-rose-500' : 'text-headline'}
+                ${props.prefix ? 'left-9' : 'left-4'} 
+                `} 
+                htmlFor={props.id} id={props.label}>
+                    {props.label}
             </label>
          </div>
      );
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,20 +1,78 @@
 import Input from "@/components/Input";
+import Image from "next/image";
+import { useCallback, useState } from "react";
+import MdEmail from 'react-icons/md'
 
 const Auth = () => {
+    const[emailValue, setEmailValue]= useState("");
+    const[usernameValue, setUsernameValue]= useState("");
+    const[passwordValue, setPasswordValue]= useState("");
+
+    const[variant, setVariant] = useState('login');
+
+    const toggleVariant= useCallback(()=>{
+        setVariant((currentVariant) => currentVariant == 'login' ? 'register': 'login')
+    }, [])
+
     return ( 
         <div className="h-full w-full relative bg-[url('/images/hero.jpg')]
             bg-no-repeat bg-center bg-fixed bg-cover ">
-            <div className="bg-black w-full h-full lg:bg-opacity-80">
+            <div className="bg-headline w-full h-full lg:bg-opacity-80">
                 <nav className="px-12 py-5">
                     <img src="/images/PinkFlix(c).png" alt="PinkFlix Logo" className="h-24" />
                 </nav>
                 <div className="flex justify-center ">
                     <div className="bg-highlight px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md
                         rounded-md w-full ">
-                        <h2 className="text-headline text-4xl mb-8 font-semibold">Sign in</h2>
+                        <h2 className="text-headline text-4xl mb-8 font-semibold">
+                           {variant == 'login' ? 'Sign In': 'Register'}
+                        </h2>
                         <div className="flex flex-col gap-4">
-                            <Input/>
+                            {variant == 'register' && ( 
+                                <Input
+                                    label="Username"
+                                    id="username"
+                                    type="username"
+                                    onChange={(ev: any)=>setUsernameValue(ev.target.value)}
+                                    value={usernameValue}
+                                    // prefix={()=>{
+                                    //     return <MdEmail size={20} className="text-headline absolute top-5 left-2" />
+                                    // }}
+                                />
+                            )}
+
+                            <Input
+                                label="Email"
+                                id="email"
+                                type="email"
+                                onChange={(ev: any)=>setEmailValue(ev.target.value)}
+                                value={emailValue}
+                                // prefix={()=>{
+                                //     return <MdEmail size={20} className="text-headline absolute top-5 left-2" />
+                                // }}
+                            />
+                            <Input
+                                label="Password"
+                                id="password"
+                                type="password"
+                                onChange={(ev: any)=>setPasswordValue(ev.target.value)}
+                                value={passwordValue}
+                                // prefix={()=>{
+                                //     return <MdEmail size={20} className="text-headline absolute top-5 left-2" />
+                                // }}
+                            />
                         </div>
+
+                        <button className="bg-headline text-primary hover:bg-paragraph rounded-md mt-10 w-full py-3 transition">
+                           {variant == 'register' ? 'Create Account': 'Log in'}
+                        </button>
+                        <p className="text-paragraph mt-12 text-center">
+                        {variant == 'login' ? 'New to PinkFlix?': 'Already have an account?'}
+                            <span onClick={toggleVariant}
+                            className="text-primary ml-1 hover:underline cursor-pointer">
+                                {variant == 'login' ? 'Create an account': 'Sign In here'}
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>
